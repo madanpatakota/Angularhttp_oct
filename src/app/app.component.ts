81fc8a65c26f3e6999afd7261a57df123e204911
Madan.patakota <madan.patakota@gmail.com>
2025-11-03 09:02:42 +0530

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angularhttp_oct';
+  constructor(private http: HttpClient) { }
+
+  fakeAPIURL           = "https://jsonplaceholder.typicode.com/todos";  // i need to get the response from this api
+
+
+ // i want to prepare my own apis as i want to learn not only get , but also post , put , delete , patch
+  myLocalfakeAPIURL    = "http://localhost:3000/Students"  // json server url
+
+  data:any = [] ;
+
+
+  //http://localhost:3000/Students
+
+  ngOnInit(): void {
+      // this.http.get(this.fakeAPIURL).subscribe( (response) => {
+      //     console.log(response);
+      //     this.data = response;
+      // })
+
+
+       this.http.get(this.myLocalfakeAPIURL).subscribe( (response) => {
+          console.log(response);
+          this.data = response;
+      })
+
+
+
+  }
+
+
+
 }
